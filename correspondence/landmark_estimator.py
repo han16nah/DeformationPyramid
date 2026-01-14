a9a72cb8c1331395d771b669e624861b550136be
han16nah <han_nah@mailbox.org>
2026-01-14 22:26:23 +0100

    visualisations with open3d, match visualisation and num_rejected per sample

--- correspondence/landmark_estimator.py
+++ correspondence/landmark_estimator.py
@@ -1,3 +1,4 @@
+    from multiprocessing.util import debug
 import torch
 import yaml
 from easydict import EasyDict as edict
@@ -32,18 +33,21 @@ class Landmark_Model ():
         self.matcher = Matcher(matcher_config).to(device)  # pretrained point cloud matcher model
         state = torch.load(config.matcher_weights, weights_only=True)
         self.matcher.load_state_dict(state['state_dict'])
+        print("Last epoch loaded for matcher:", state['epoch'])
 
         # outlier model initialization
         self.outlier_model = Outlier_Rejection(outlier_rejection_config.model).to(device)
         state = torch.load(config.outlier_rejection_weights, weights_only=True)
         self.outlier_model.load_state_dict(state['state_dict'])
-
+        print("Last epoch loaded for outlier model:", state['epoch'])
         self.device = device
 
         self.kpfcn_config = config['kpfcn_config']
+        # for debugging
+        self.vis = True
 
 
-    def inference(self, inputs, reject_outliers=True, inlier_thr=0.5, timer=None):
+    def inference(self, inputs, reject_outliers=True, inlier_thr=0.8, timer=None):
 
         self.matcher.eval()
         self.outlier_model.eval()
@@ -62,8 +66,11 @@ class Landmark_Model ():
             coarse_flow = data['coarse_flow'][0]
             inlier_mask, inlier_rate = NeCoLoss.compute_inlier_mask(data, inlier_thr, s2t_flow=coarse_flow)
             match_filtered = inlier_mask[0] [  inlier_conf > inlier_thr ]
+            # plot
+            if self.vis:
+                plot(data, inlier_mask[0])
+            
             inlier_rate_2 = match_filtered.sum()/(match_filtered.shape[0])
-
             vec_6d = data['vec_6d'][0]
 
             if reject_outliers:
@@ -73,3 +80,54 @@ class Landmark_Model ():
 
 
             return ldmk_s, ldmk_t, inlier_rate, inlier_rate_2
+    
+
+def plot(data, inlier_mask=None):
+    from correspondence.lib.benchmark_utils import correspondence_viz_open3d
+    import numpy as np
+    
+    s_pcd_raw = data ['src_pcd_list'][0]
+    t_pcd_raw = data ['tgt_pcd_list'][0]
+    s_pcd, t_pcd = data['s_pcd'][0], data['t_pcd'][0]
+    s2t_flow = data['coarse_flow'][0]
+    match_pred = data['coarse_match_pred']
+    batched_rot = data['batched_rot']  # B,3,3
+    batched_trn = data['batched_trn']
+
+    # use the match prediction as the motion anchor
+    match_pred_i = match_pred[ match_pred[:, 0] == 0 ]
+    s_id , t_id = match_pred_i[:,1], match_pred_i[:,2]
+    t_pcd_matched= t_pcd[t_id]
+    
+    # Transform source points: deform + rigid transform
+    s_pcd_deformed = s_pcd + s2t_flow  # Apply deformation
+    s_pcd_wrapped = (batched_rot[0] @ s_pcd_deformed.T + batched_trn[0]).T  # Apply rigid transform
+    
+    # Get transformed source points and target points
+    s_pcd_matched_transformed = s_pcd_wrapped[s_id]  # In target frame!
+    t_pcd_matched = t_pcd[t_id]  # Already in target frame
+
+    # Compute inliers
+    inlier_thr = 0.04
+    distances_sq = torch.sum((s_pcd_matched_transformed - t_pcd_matched)**2, dim=1)
+    inliers = distances_sq < (inlier_thr**2)
+
+    s_id , t_id = match_pred[:,1], match_pred[:,2]
+    corrs = np.stack([s_id.cpu().numpy(), t_id.cpu().numpy()], axis=0)
+
+    correspondence_viz_open3d(s_pcd_raw, t_pcd_raw, s_pcd, t_pcd, corrs, inlier_mask=inliers)
+    
+    if inlier_mask is not None:
+        # Convert to numpy if it's a tensor
+        if torch.is_tensor(inlier_mask):
+            inlier_mask = inlier_mask.cpu().numpy()
+        
+        n_corrs = corrs.shape[1]
+        # filter correspondences and inliers with the mask
+        corrs = corrs[:, inlier_mask]
+        inliers = inliers[inlier_mask]
+
+        # report how many correspondences were filtered
+        print(f"{n_corrs - inlier_mask.sum()}/{n_corrs} correspondences filtered out.")
+
+    correspondence_viz_open3d(s_pcd_raw, t_pcd_raw, s_pcd, t_pcd, corrs, inlier_mask=inliers)
