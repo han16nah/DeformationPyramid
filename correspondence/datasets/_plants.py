96dbffabcf99048feee9a2680f58a736414639f3
han16nah <han_nah@mailbox.org>
2026-01-24 09:18:14 +0100

    remove assertion for empty outliers

--- correspondence/datasets/_plants.py
+++ correspondence/datasets/_plants.py
@@ -111,10 +111,10 @@ class _Plants(Dataset):
         src_pcd_deformed = src_pcd + s2t_flow
         if downsampled:
             correspondences = find_new_corr(correspondences, sub_idx_src, sub_idx_tgt)
-            # assert that none of the important variables are empty
+            # assert that none of the important variables are empty 
             assert src_pcd.shape[0] > 0, "Source point cloud is empty after downsampling."
             assert tgt_pcd.shape[0] > 0, "Target point cloud is empty after downsampling."
-            assert correspondences.shape[0] > 0, "Correspondences are empty after downsampling."
+            # assert correspondences.shape[0] > 0, "Correspondences are empty after downsampling."  # removed; empty corres may be good for the outlier rejection?
             assert s2t_flow.shape[0] > 0, "Scene flow is empty after downsampling."
 
         
